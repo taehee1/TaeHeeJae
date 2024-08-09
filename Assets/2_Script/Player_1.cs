277efb9b5888c047aab0f1557b3ba72ac2503014
taehee1 <kt1430@naver.com>
2024-08-09 17:49:35 +0900

    Initial commit

---
+++ Assets/2_Script/Player_1.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_1 : MonoBehaviour
+{
+    public float moveSpeed;
+    public float jumpPower;
+
+    private float moveX;
+
+    private Rigidbody2D rb;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        Move();
+        Jump();
+    }
+
+    private void Move()
+    {
+        moveX = Input.GetAxisRaw("Horizontal");
+
+        rb.velocity = new Vector2(moveX * moveSpeed, rb.velocity.y);
+    }
+
+    private void Jump()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            rb.velocity = new Vector2(rb.velocity.x, jumpPower);
+        }
+    }
+}
