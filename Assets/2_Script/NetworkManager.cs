7e47f1a769f116e00bb0633812d906c613993156
leechuanhung <163071930+taehee1@users.noreply.github.com>
2024-08-20 10:54:39 +0900

    서버연결/닉네임

--- Assets/2_Script/NetworkManager.cs
+++ Assets/2_Script/NetworkManager.cs
@@ -3,27 +3,49 @@ using System.Collections.Generic;
 using UnityEngine;
 using Photon.Pun;
 using Photon.Realtime;
+using UnityEngine.UI;
 
 public class NetworkManager : MonoBehaviourPunCallbacks
 {
-    public GameObject connectPanel;
-    public GameObject connectingTxt;
-    public GameObject connectedTxt;
-
+    public InputField nicknameInput;
+    public Text networkSituation;
 
     private void Awake()
     {
-        Screen.SetResolution(1920, 1080, false);
-        Connect();
+        Screen.SetResolution(960, 540, false);
+        nicknameInput.text = null;
+    }
+
+    private void Update()
+    {
+        networkSituation.text = PhotonNetwork.NetworkClientState.ToString();
     }
 
     public void Connect()
     {
-        PhotonNetwork.ConnectUsingSettings();
+        if (nicknameInput.text != null)
+        {
+            PhotonNetwork.ConnectUsingSettings();
+        }
+        else
+        {
+            print("닉네임을 입력해주세요");
+        }
     }
 
     public override void OnConnectedToMaster()
     {
         print("서버연결완료");
+        PhotonNetwork.LocalPlayer.NickName = nicknameInput.text;
+    }
+
+    public void Disconnect()
+    {
+        PhotonNetwork.Disconnect();
+    }
+
+    public override void OnDisconnected(DisconnectCause cause)
+    {
+        print("연결끊김");
     }
 }
